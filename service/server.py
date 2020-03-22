2e5b4652e4b2223fcb4b2c4c2d41a59b9e8f6734
ARQtty <ShamovIA@yandex.ru>
2020-03-22 16:02:15 +0300

    Handle errors, fix request parameters, return real price (not logarithm)

--- service/server.py
+++ service/server.py
@@ -1,4 +1,4 @@
-from math import log1p, log, sqrt
+from math import log1p, log, sqrt, exp
 from flask import Flask, request, jsonify
 from catboost import CatBoostRegressor
 
@@ -6,13 +6,13 @@ app = Flask(__name__)
 
 
 def preprocess_data(request):
-    size = request.args.get('size')
-    mass = float(request.args.get('mass'))
-    material = request.args.get('material')
-    #sum_ops_time = float(request.args.get('sum_ops_time'))
+    sep = '/'
+    size = request.form.get('size').lower().replace('х', sep).replace('x', sep)
+    mass = float(request.form.get('mass').replace(',', '.'))
+    material = request.form.get('material')
 
     mul = lambda arr: arr[0] * mul(arr[1:]) if len(arr) > 1 else arr[0]
-    calc_dims = lambda s: list([float(x) for x in s.lower().split('х')])
+    calc_dims = lambda s: list([float(x) for x in s.split(sep)])
     get_material = lambda s: s.split()[0].lower()
     materials = {'лист': 1,
                  'рулон': 2,
@@ -24,7 +24,6 @@ def preprocess_data(request):
     log_volume = log1p(mul(calc_dims(size)))
     log_mass = log(mass)
     sqrt_mass = sqrt(mass)
-    #sum_ops_time = sum_ops_time
     density = mass / mul(calc_dims(size))  # mass / volume
     material_category = materials[get_material(material)]
 
@@ -42,10 +41,20 @@ def calc_price():
     # current model columns
     # ['size2', 'size3', 'log_volume', 'log_mass', 'sqrt_mass', 'sum_time', 'density', 'material_category']
 
-    x = preprocess_data(request)
-    pred = model.predict(x)
+    params = ('size', 'mass', 'material')
+    if not all([item in request.form for item in params]):
+        return jsonify({'error': 'not enough detail parameters in request', 'price': None})
+ 
+    try:
+        x = preprocess_data(request)
+    except ValueError:
+        return jsonify({'error': 'invalid detail data format', 'price': None})
+    except KeyError:
+        return jsonify({'error': 'invalid detail data. Unknown material', 'price': None})
 
-    return jsonify({'price': pred})
+    pred = model.predict(x)
+    price = round(exp(pred), 2)
+    return jsonify({'price': price})
 
 
 if __name__ == "__main__":
