d641fdb54fe4d5d25d0f797bc491a08aee5d6a7a
tanyapole <tanusha2406@gmail.com>
2020-11-25 01:24:08 +0300

    Predict profilnaya rezka

--- service/predict_norms/api.py
+++ service/predict_norms/api.py
@@ -1,10 +1,11 @@
 import numpy as np
-import cv2
 import torch
 import pickle
-import predict_operations.detection
 from predict_norms.model import ConvModel
+from predict_norms import common
+from predict_norms.predict_profilnaya import predict_profilnaya
 
+_profilnaya_operation = 'профильно-вырезная электрофизическая лучевая лазерная'
 
 model_pt = './predict_norms/predict_norms.pth'
 with open ('./predict_norms/norms_operations.pkl', 'rb') as f:
@@ -19,16 +20,6 @@ with open('./predict_norms/predict_ops.pkl', 'rb') as f:
 with open('./predict_norms/predict_ops_massdes.pkl', 'rb') as f:
     model_predict_ops_massdes = pickle.load(f)
 
-
-
-def _extract_projections(img):
-    return predict_operations.detection.crop_conturs(img)
-
-def _preprocess(img):
-    img = cv2.resize(img / 255., (150,150))
-    return torch.tensor(img, dtype=torch.float32).unsqueeze(0)
-
-def _np(t): return t.cpu().detach().numpy()
 
 def _convert_detail_name_to_ohe(curdetail, detail_names):
     r = np.zeros(len(detail_names), dtype=np.uint8)
@@ -46,7 +37,6 @@ def predict_operations(detail_name:str, mass:float, thickness:float, mass_des:fl
     Returns: list of operations if successful else `None`
     """
     try:
-        print(all_details)
         detail = _convert_detail_name_to_ohe(detail_name, all_details)
         X = np.concatenate((detail, np.array([mass]), np.array([thickness])), axis=0)
         if mass_des is not None:
@@ -57,19 +47,26 @@ def predict_operations(detail_name:str, mass:float, thickness:float, mass_des:fl
     except:
         return None
 
-def predict_norms(img):
+def predict_norms(img, detail_name:str, mass:float, thickness:float, length:float, width:float):
     '''
     Args:
         img: grayscale image array with values in range 0..255
+        length: больший из габаритов
+        width: меньший из габаритов
     returns: operations and corresponing probabilities if successful else `None`
     '''
     try:
-        projections = list(map(_preprocess, _extract_projections(img)))
+        inp = common.img_to_model_input(img)
         with torch.no_grad():
-            output = model_predict_norms([projections])
-        output = np.clip(_np(output), a_min=0, a_max=None)
-        return {
+            output = model_predict_norms(inp)
+        output = np.clip(common.np(output), a_min=0, a_max=None)
+        res = {
             operations[i]: output[0][i] for i in range(len(operations))
         }
+        try:
+            res[_profilnaya_operation] = predict_profilnaya(img, mass, detail_name, thickness)
+        except:
+            pass
+        return res
     except:
         return None
---
+++ service/run_tests.py
@@ -0,0 +1,3 @@
+from unittests import predict_norms_tests
+
+predict_norms_tests.main()
